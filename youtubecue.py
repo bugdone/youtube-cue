41ba2f7d3c2b92289b58ec80630c5b1f1e2d841d
bugdone <bugdone@gmail.com>
2017-11-10 17:15:56 +0200

    Add youtubecue_log_dir environment variable to set logging directory

--- youtubecue.py
+++ youtubecue.py
@@ -1,8 +1,11 @@
 import json
 import sys
+import os
 
 import youtubecue
 
 
 if __name__ == '__main__':
+    if 'youtubecue_log_dir' in os.environ:
+        youtubecue.set_log_directory(os.environ.get('youtubecue_log_dir'))
     print json.dumps(youtubecue.get_cue(sys.argv[1]), indent=4)
